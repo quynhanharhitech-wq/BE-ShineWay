a2a794419896cb2c536567e83046ec27d63c0c9d
nhat.truong <truongnhat2102@gmail.com>
2025-10-08 17:32:24 +0700

    feat/create base src

---
+++ log-service/src/test/java/com/shineway/log_service/LogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shineway.log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ menu-service/src/test/java/com/shineway/menu_service/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shineway.menu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
